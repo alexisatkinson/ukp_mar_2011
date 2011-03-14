797226048bca1fa1fde3966188846f6c7e7c64ec
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-03-14 13:05:50 -0400

    Updated the Where namespaces

--- product/nothinbutdotnetprep/utility/filtering/Where.cs
+++ product/nothinbutdotnetprep/utility/filtering/Where.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using System;
+
+namespace nothinbutdotnetprep.utility.filtering
+{
+    public static class Where<ItemToFilter>
+    {
+        public static object has_a<ReturnType>(PropertyAccessor<ItemToFilter, ReturnType> property_accessor)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
