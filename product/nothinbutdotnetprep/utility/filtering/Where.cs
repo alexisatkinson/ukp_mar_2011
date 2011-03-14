92589db443ef6907e84677d20ff6aff161b61bba
Ronan Moriarty <ronan.moriarty@hotmail.co.uk>
2011-03-14 17:19:29 +0000

    Pushing new changes

--- product/nothinbutdotnetprep/utility/filtering/Where.cs
+++ product/nothinbutdotnetprep/utility/filtering/Where.cs
@@ -1,12 +1,25 @@
-﻿using System;
-
-namespace nothinbutdotnetprep.utility.filtering
+﻿namespace nothinbutdotnetprep.utility.filtering
 {
-    public static class Where<ItemToFilter>
-    {
-        public static object has_a<ReturnType>(PropertyAccessor<ItemToFilter, ReturnType> property_accessor)
-        {
-            throw new NotImplementedException();
-        }
-    }
+	public static class Where<ItemToFilter>
+	{
+		public static SpecificationFactory<ItemToFilter, ReturnType> has_a<ReturnType>(PropertyAccessor<ItemToFilter, ReturnType> property_accessor)
+		{
+			return new SpecificationFactory<ItemToFilter, ReturnType>(property_accessor);
+		}
+	}
+
+	public class SpecificationFactory<ItemToFilter, ReturnType>
+	{
+		private readonly PropertyAccessor<ItemToFilter, ReturnType> propertyAccessor;
+
+		public SpecificationFactory(PropertyAccessor<ItemToFilter, ReturnType> propertyAccessor)
+		{
+			this.propertyAccessor = propertyAccessor;
+		}
+
+		public Criteria<ItemToFilter> equal_to(ReturnType valueToEqual)
+		{
+			return new ConditionalCriteria<ItemToFilter>(m => propertyAccessor(m).Equals(valueToEqual));
+		}
+	}
 }
