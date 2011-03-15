28d4c6a14043e4d8f8fe7226efba09caf7aea7e2
unknown <Carl Bradbury@.(none)>
2011-03-15 11:53:11 +0000

    Pushing new changes

--- product/nothinbutdotnetprep/utility/filtering/DefaultCriteriaFactory.cs
+++ product/nothinbutdotnetprep/utility/filtering/DefaultCriteriaFactory.cs
@@ -1,4 +1,6 @@
-﻿namespace nothinbutdotnetprep.utility.filtering
+﻿using System;
+
+namespace nothinbutdotnetprep.utility.filtering
 {
     public class DefaultCriteriaFactory<ItemToFilter, ReturnType> : CriteriaFactory<ItemToFilter, ReturnType>
     {
@@ -14,10 +16,16 @@
             return equal_to_any(value_to_equal);
         }
 
+        public CriteriaFactory<ItemToFilter, ReturnType> not
+        {
+            get { return new NotCriteriaFactory<ItemToFilter, ReturnType>(this); }
+        }
+
         public Criteria<ItemToFilter> equal_to_any(params ReturnType[] values)
         {
             return create_from(new IsEqualToAny<ReturnType>(values));
         }
+
 
         public Criteria<ItemToFilter> not_equal_to(ReturnType value)
         {
@@ -28,6 +36,31 @@
         {
             return new PropertyCriteria<ItemToFilter, ReturnType>(property_accessor,
                                                                   property_criteria);
+        }
+    }
+
+    public class NotCriteriaFactory<ItemToFilter, ReturnType> : CriteriaFactory<ItemToFilter, ReturnType>
+    {
+        private DefaultCriteriaFactory<ItemToFilter, ReturnType> criteriaFactory;
+
+        public NotCriteriaFactory(DefaultCriteriaFactory<ItemToFilter, ReturnType> criteriaFactory)
+        {
+            this.criteriaFactory = criteriaFactory;
+        }
+
+        public Criteria<ItemToFilter> create_from(Criteria<ReturnType> property_criteria)
+        {
+            return new NotCriteria<ItemToFilter>(criteriaFactory.create_from(property_criteria));
+        }
+
+        public Criteria<ItemToFilter> equal_to(ReturnType value_to_equal)
+        {
+            return new NotCriteria<ItemToFilter> ( criteriaFactory.equal_to(value_to_equal));
+        }
+
+        public Criteria<ItemToFilter> equal_to_any(params ReturnType[] values)
+        {
+            return new NotCriteria<ItemToFilter>(criteriaFactory.equal_to_any(values));
         }
     }
 }
